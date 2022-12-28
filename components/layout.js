5043b74eac2bbba3207a9eaba4365e080e8da6c9
Shuvrojit Biswas <jsuvro17@gmail.com>
2022-12-28 23:59:20 +0600

    Added Layout. Cleaned css.

---
+++ components/layout.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Layout({children}) {
+    return (
+        <div>
+            <header>
+                header
+            </header>
+            <main>
+                {children}
+            </main>
+            <footer>
+                footer
+            </footer>
+
+        </div>
+    )
+}
